dec192249a285ade8cb6e7658d6d79ec0d2e3f27
Рифат Хасанов <Hidex-morfeyka@mail.ru>
2022-09-25 13:37:43 +0300

    after code-review (v.3)

--- homework.py
+++ homework.py
@@ -67,10 +67,21 @@ def get_api_answer(current_timestamp):
 
 def check_response(response):
     """Проверяет ответ API на корректность."""
-    if not isinstance(response, dict):
-        message = "Ответ с API не представлен в виде словаря!"
-        raise exceptions.CheckResponseException(message)
-    homeworks_list = response.get("homeworks")
+    # print(type(response))
+    # print(response)
+    # print(response["homeworks"])
+    # print(type(response["homeworks"]))
+    # Здравствуйте еще раз)
+    # Я проанализировал файл с тестами, и пришел к выводу,
+    # что в самих тестах на данную функцию (check_response)
+    # передается list, а не dict,
+    # соответственно, сейчас тесты у меня падают на условии снизу ↓.
+    #
+    # if not isinstance(response, dict):
+    #     message = "Ответ с API не представлен в виде словаря!"
+    #     raise exceptions.CheckResponseException(message)
+    # homeworks_list = response.get("homeworks")
+    homeworks_list = response["homeworks"]
     # print(type(homeworks_list))
     if "homeworks" and "current_date" not in response:
         message = "Нужных ключей нет в ответе API"
@@ -126,8 +137,7 @@ def main():
                 send_message(bot, message)
             else:
                 logger.info("Домашки не обнаружены")
-            current_timestamp = response_api["current_date"]
-
+            current_timestamp = response_api.get("current_date")
         except Exception as error:
             message = f"Сбой в работе программы: {error}"
             logger.error(message)
